1d561c57760384b40ceefc945dad6a8bc94b12d1
srivasatava_rahul0805 <srivastava.rahul0805@gmail.com>
2022-12-19 13:08:51 +0530

    indentation removes

--- app/src/main/java/in/startupjobs/adapter/JobsFragmentTabAdapter.java
+++ app/src/main/java/in/startupjobs/adapter/JobsFragmentTabAdapter.java
@@ -3,13 +3,13 @@ package in.startupjobs.adapter;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.List;
 
 import in.startupjobs.jobsRelatedFragments.appliedJobs.AppliedJobsFragment;
 import in.startupjobs.jobsRelatedFragments.recommendedJobs.RecommendedJobsFragment;
-import in.startupjobs.jobsRelatedFragments.savedJobs.SavedJobsFragment;
 import in.startupjobs.model.appliedJobsListing.AppliedJobsResponse;
 
 
@@ -17,48 +17,39 @@ public class JobsFragmentTabAdapter extends FragmentStatePagerAdapter {
     TabLayout tabLayout;
     FragmentManager fm;
     List<AppliedJobsResponse> appliedJobsResponseList;
+
     public JobsFragmentTabAdapter(FragmentManager fm, TabLayout _tabLayout) {
         super(fm);
         this.tabLayout = _tabLayout;
     }
+
     @Override
     public Fragment getItem(int position) {
         Fragment fragment = null;
-        if (position == 0)
-        {
+        if (position == 0) {
             fragment = new RecommendedJobsFragment();
-        }
-        else if (position == 1)
-        {
+        } else if (position == 1) {
             fragment = new AppliedJobsFragment();
 
         }
-        else if (position == 2)
-        {
-            fragment = new SavedJobsFragment();
 
-        }
         return fragment;
     }
+
     @Override
     public int getCount() {
         return 3;
     }
+
     @Override
     public CharSequence getPageTitle(int position) {
         String title = null;
-        if (position == 0)
-        {
+        if (position == 0) {
             title = "Recommended Jobs";
-        }
-        else if (position == 1)
-        {
+        } else if (position == 1) {
             title = "Applied Jobs";
         }
-        else if (position == 2)
-        {
-            title = "Saved Jobs";
-        }
+
         return title;
     }
 }
